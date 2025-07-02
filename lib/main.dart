cfb77370aa473bdeb3c6520b4f7497e451b5e48e
TrevorRomans <143967141+TrevorRomans@users.noreply.github.com>
2025-07-02 10:25:09 -0700

    Completed login screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 // TODO: Developers, add your screens below here
-//Updated the login screen, should be better now :)
+//Login screen now completed
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
